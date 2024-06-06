90be2176b14b61eb108b8fec991444e0f67ce594
Paim22 <90618922+Paim22@users.noreply.github.com>
2024-06-06 12:44:44 -0300

    Add files via upload

---
+++ triste_fim_policarpo.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Área Restrita</title>
+    <link rel="stylesheet" href="style/livro.css">
+    <link rel="shortcut icon" href="imagens/icon/paz.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<style>
+        p#resumo7 {
+            color: white;
+        }
+    </style>
+
+<div id="content">
+    <!-- Seu conteúdo existente -->
+
+    <h2>Resumo do Livro "Morte e Vida Severina"</h2>
+    <p>
+    <p id="resumo7">
+    "Triste Fim de Policarpo Quaresma" é um romance do escritor brasileiro Lima Barreto, publicado em 1911. A obra narra a história de Policarpo Quaresma, um nacionalista patriótico e ingênuo que sonha em promover reformas e valorizar a cultura brasileira. 
+    Policarpo é um funcionário público singular que acredita no potencial do Brasil e defende mudanças radicais para fortalecer a identidade nacional. Ele se dedica a diversos projetos de patriotismo, como o estudo do tupi-guarani, o plantio de árvores nacionais e a valorização da cultura nacional, mas é ridicularizado e marginalizado pelas autoridades e pela sociedade.
+    A narrativa explora temas como nacionalismo, patriotismo, burocracia, corrupção, e a relação entre ideais e realidade. A trajetória de Policarpo ilustra as contradições e dificuldades enfrentadas por aqueles que buscam transformações sociais e culturais em um contexto adverso.
+    </p>
+</div>
+
+<div id="cortes">
+    <a href="sair.php">Sair</a>
+</div>
+
+<div id="cortes">
+    <a href="areaPrivada.php">Area Privada</a>
+</div>
+
+</body>
+<footer>
+    <p>&copy; Todos os direitos reservados</p>
+</footer>
+</html>
