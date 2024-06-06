0a582cc9a0396e16dcfe1742c4f5e55f298a6921
Paim22 <90618922+Paim22@users.noreply.github.com>
2024-06-06 12:43:45 -0300

    Add files via upload

---
+++ quinze.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Área Restrita</title>
+    <link rel="stylesheet" href="style/livro.css">
+    <link rel="shortcut icon" href="imagens/icon/paz.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<style>
+        p#resumo5 {
+            color: white;
+        }
+    </style>
+
+
+<div id="content">
+    <!-- Seu conteúdo existente -->
+
+    <!-- Resumo do livro "O Cortiço" -->
+    <h2>Resumo do Livro "O Cortiço"</h2>
+    <p id="resumo5">
+    "O Quinze" é um romance regionalista que retrata a seca no sertão nordestino do Brasil, especificamente no estado do Ceará, durante a chamada "tragédia das secas" de 1915. A obra destaca as duras condições enfrentadas pelo povo sertanejo e as consequências sociais, econômicas e emocionais da seca.
+    No livro, Rachel de Queiroz narra as histórias de Conceição, uma jovem mulher de classe média que enfrenta as mudanças em sua vida devido à seca, e de Chico Bento, um vaqueiro que lida com os desafios da sobrevivência em um cenário tão árido.
+    A narrativa repleta de realismo e sensibilidade descreve a luta pela sobrevivência, a solidariedade entre as comunidades e as relações humanas em meio à adversidade, mostrando as diferentes perspectivas e a força do povo nordestino diante de uma das maiores tragédias naturais da região.
+    </p>
+</div>
+
+<div id="cortes">
+    <a href="sair.php">Sair</a>
+</div>
+
+<div id="cortes">
+    <a href="areaPrivada.php">Area Privada</a>
+</div>
+
+</body>
+<footer>
+    <p>&copy; Todos os direitos reservados</p>
+</footer>
+</html>
