cb108c2f5836c81d90be43dce332e96d77f4f7fc
Paim22 <90618922+Paim22@users.noreply.github.com>
2024-06-06 12:44:16 -0300

    Add files via upload

---
+++ romeu_julieta.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Área Restrita</title>
+    <link rel="stylesheet" href="style/livro.css">
+    <link rel="shortcut icon" href="imagens/icon/paz.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<style>
+        p#resumo6 {
+            color: white;
+        }
+    </style>
+
+<div id="content">
+    <!-- Seu conteúdo existente -->
+
+    <!-- Resumo do livro "O Cortiço" -->
+    <h2>Resumo do Livro "O Cortiço"</h2>
+    <p id="resumo6">
+    "Romeu e Julieta" é uma das obras mais conhecidas e aclamadas de Shakespeare, representando uma das maiores histórias de amor trágico da literatura. A peça conta a história de dois jovens amantes, Romeu Montecchio e Julieta Capuleto, cujas famílias estão em conflito.
+    No centro da trama está o romance proibido entre Romeu e Julieta, que enfrentam obstáculos significativos devido ao ódio entre suas famílias. O casal secreta-se amorosamente e, apesar dos desafios, decide se casar em segredo. No entanto, um mal-entendido leva a consequências trágicas, culminando em um desfecho devastador.
+    A peça aborda temas como o poder do destino, o conflito entre o amor e o ódio, a juventude e a impulsividade. A paixão intensa e a tragédia de "Romeu e Julieta" continuam a cativar o público até os dias de hoje, tornando-a atemporal e uma das obras mais reverenciadas da literatura mundial.
+    </p>
+</div>
+
+<div id="cortes">
+    <a href="sair.php">Sair</a>
+</div>
+
+<div id="cortes">
+    <a href="areaPrivada.php">Area Privada</a>
+</div>
+
+</body>
+<footer>
+    <p>&copy; Todos os direitos reservados</p>
+</footer>
+</html>
